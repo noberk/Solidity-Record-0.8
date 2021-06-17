33f8c5f96df20c62570f39a721c9f1f56e05b08f
noberk <noberk@users.noreply.github.com>
2021-06-17 18:25:49 +0800

    add formatter

--- src/solidity/basic/types/Struct.sol
+++ src/solidity/basic/types/Struct.sol
@@ -1,12 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.5;
 
-struct Point{
-    uint x;
-    uint y;
-}
+contract Area {
+    struct Point {
+        uint256 x;
+        uint256 y;
+    }
+    Point public point = Point({x: 1, y: 2});
 
-contract Area{
-    
-    
+    Point[] public pointArray;
+
+    function fillPoints(uint256 count) external {
+        for (uint256 index = 0; index < count; index++) {
+            pointArray.push(Point(index, ++index));
+        }
+    }
+
+    function getCount() external view returns (uint256) {
+        return pointArray.length;
+    }
+
+    function getArray() external view returns (Point[] memory) {
+        return pointArray;
+    }
 }
